827713e483833389e6f97df69d6d88f9739a333e
GP4cK <gautier.bayzelon@gmail.com>
2023-12-09 13:22:52 +0800

    🎉 day 09

---
+++ lib/day09/part2.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main() async {
+  final file = File('./inputs/09.txt');
+  final lines = await file.readAsLines();
+  final values = <int>[];
+  for (final line in lines) {
+    final subLines = [
+      line.split(' ').map((value) => int.parse(value)).toList()
+    ];
+    var allZero = false;
+    while (!allZero) {
+      final newLine = <int>[];
+      final lastLine = subLines.last;
+      for (var i = 0; i < lastLine.length - 1; i++) {
+        newLine.add(lastLine[i + 1] - lastLine[i]);
+      }
+      subLines.add(newLine);
+      allZero = newLine.every((value) => value == 0);
+    }
+    var value = 0;
+    for (var i = subLines.length - 1; i >= 0; i--) {
+      value = subLines[i].first - value;
+    }
+
+    values.add(value);
+  }
+  print(values.fold<int>(0, (acc, value) => acc + value));
+}
